b1ef34014ba8725e619722922f85ba9d66a7e96c
evilduckling <ttsteam@hotmail.com>
2016-09-24 08:46:02 +0200

    Login interface + login call

--- app/src/main/java/com/evilduckling/nainmailer/MainActivity.java
+++ app/src/main/java/com/evilduckling/nainmailer/MainActivity.java
@@ -1,13 +1,59 @@
 package com.evilduckling.nainmailer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.evilduckling.nainmailer.interfaces.Const;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText login;
+    private EditText password;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (EditText) findViewById(R.id.login);
+        password = (EditText) findViewById(R.id.password);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AsyncHttpClient client = new AsyncHttpClient();
+                RequestParams rp = new RequestParams();
+                rp.put("login", login.getText().toString());
+                rp.put("password", password.getText().toString());
+
+                client.post(MainActivity.this, "http://nainwak.com/index.php", rp, new TextHttpResponseHandler() {
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        Toast.makeText(MainActivity.this, "Sorry something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                        Log.e(Const.LOG_TAG, responseString);
+                    }
+                });
+
+            }
+        });
+
     }
 }

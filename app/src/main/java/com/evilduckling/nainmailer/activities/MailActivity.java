2fe1bf6cb49b36073dd7aa768a070d61a5129a8a
evilduckling <renaud.cammarata@gmail.com>
2016-09-27 20:14:00 +0200

    clean code

--- app/src/main/java/com/evilduckling/nainmailer/activities/MailActivity.java
+++ app/src/main/java/com/evilduckling/nainmailer/activities/MailActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.evilduckling.nainmailer.R;
@@ -25,9 +23,6 @@ import cz.msebera.android.httpclient.Header;
 
 public class MailActivity extends AppCompatActivity {
 
-    private TextView allMail;
-    private TextView unreadMail;
-    private Button refresh;
     private ListView list;
 
     @Override
@@ -35,74 +30,13 @@ public class MailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail);
 
-/*        allMail = (TextView) findViewById(R.id.mail_all);
-        unreadMail = (TextView) findViewById(R.id.mail_unread);
-        refresh = (Button) findViewById(R.id.mail_refresh_button);*/
         list = (ListView) findViewById(R.id.mail_list);
-
-  /*      refresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getMailData();
-            }
-        });*/
-
-        // getMailData();
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         getFullInbox();
-    }
-
-    private void getMailData() {
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(this, "http://nainwak.com/jeu/chat.php?IDS=" + getIdentifier(), new TextHttpResponseHandler() {
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                Log.e(Const.LOG_TAG, "" + responseString);
-                Toast.makeText(MailActivity.this, "Sorry something went wrong", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(MailActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                Log.d(Const.LOG_TAG, "" + responseString);
-                if (!tryToExtractData(responseString)) {
-                    Toast.makeText(MailActivity.this, "Session must have expired", Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(MailActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
-    }
-
-    private boolean tryToExtractData(String responseString) {
-
-        Pattern extractorPattern = Pattern.compile("^.*<span class=\"event-temps\">(.*)messages, <span class=\"chatpager.*lu\">(.*)non lus</span>.*$");
-
-        String noReturnString = responseString
-            .replaceAll("\n", "")
-            .replaceAll("\r", "");
-
-        Matcher m = extractorPattern.matcher(noReturnString);
-        if (m.matches()) {
-
-            String all = m.group(1).trim();
-            String unread = m.group(2).trim();
-
-            allMail.setText(all + " messages au total");
-            unreadMail.setText(unread + " messages non lus");
-
-            return true;
-        }
-
-        return false;
     }
 
     private String getIdentifier() {
@@ -135,9 +69,9 @@ public class MailActivity extends AppCompatActivity {
 
         Pattern extractorPattern = Pattern.compile("^.*<td><a class=\"(.*)\" href=\"viewchat.php\\?IDS=.*&amp;id=(\\d*)&amp;page=in\"><b>.* : (.*)</b> : <i>(.*)</i></a></td>.*$");
 
-        String noReturnString = responseString.replaceAll("\r", "").replaceAll("\n", "@##@@##@");
+        String noReturnString = responseString.replaceAll("\r", "").replaceAll("\n", "@99##@@632##@");
 
-        String[] exploded = explode(noReturnString, "@##@@##@");
+        String[] exploded = explode(noReturnString, "@99##@@632##@");
 
         for (String chunk : exploded) {
 
